6862ae8712ff56888d926ccbf1a14dfddccdf5ee
Luiz Henrique Cunha <lhcunha1@gmail.com>
2024-02-19 09:18:48 -0300

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,3 +1,5 @@
+from alphabet import alphabet
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
