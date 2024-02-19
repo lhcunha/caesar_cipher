d5b48e54651e052ee3110774c0a315f2b83033dd
Luiz Henrique Cunha <lhcunha1@gmail.com>
2024-02-19 09:17:17 -0300

    Create cipher.py

---
+++ cipher.py
@@ -0,0 +1,23 @@
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def cypher(text, shift):
+  new_text = ""
+  for letter in text:
+    if letter == " ":
+      new_text += " "
+    elif letter not in alphabet:
+      new_text += letter
+    else:
+      if direction == "encode":
+        new_index = alphabet.index(letter) + shift
+      elif direction == "decode":
+        new_index = alphabet.index(letter) - shift
+      else:
+        print(f"{direction} is not a valid option!")
+      letter = alphabet[new_index]
+      new_text += letter
+  print(f"The encoded text is: {new_text}")
+
+cypher(text, shift)
